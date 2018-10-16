049914db6651dbf619ba2bc07bceebd7387af855
diegogcc <diego.castro@getpreo.com>
2018-10-16 16:53:38 -0500

    Apps lifecycle methods added to viewcontroller.swift

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -20,6 +20,26 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         nameTextField.delegate = self
         
     }
+    override func loadView() {
+        super.loadView()
+        print("Se activo la func loadView")
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        print("Se activo la func viewWillAppear")
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        print("Se activo la func viewDidAppear")
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        print("Se activo la func viewWillDisappear")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(true)
+        print("Se activo la func viewDidDisappear")
+    }
     //MARK: UITextFieldDelegate
     
     
