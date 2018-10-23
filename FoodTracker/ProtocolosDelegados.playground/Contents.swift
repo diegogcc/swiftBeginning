97b17d5d69510f2f7b9cd94b9b5be21c8e63e44a
diegogcc <diego.castro@getpreo.com>
2018-10-23 16:57:02 -0500

    Protocols and Delegates

---
+++ FoodTracker/ProtocolosDelegados.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+protocol operacionesBasicas{
+    func Suma()
+    func Resta()
+    func Multiplicacion()
+    func Division()
+}
+
+class Calculadora:operacionesBasicas{
+    func Suma() {
+        
+    }
+    func Resta() {
+        
+    }
+    func Multiplicacion() {
+        
+    }
+    func Division() {
+        
+    }
+}
